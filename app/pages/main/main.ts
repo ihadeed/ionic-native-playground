d9f5354139f8f6745e5f8f2aa948176797739654
Ibrahim Hadeed <ibby93@gmail.com>
2016-03-07 13:09:41 -0500

    feat: import plugins from ionic-native library

--- app/pages/main/main.ts
+++ app/pages/main/main.ts
@@ -1,5 +1,24 @@
 import {Page} from 'ionic-angular';
-
+// Import used plugins from Ionic Native library
+import {
+ActionSheet,
+AppRate,
+AppVersion,
+Badge,
+BarcodeScanner,
+//BLE,
+Calendar,
+Camera,
+Contacts,
+DatePicker,
+Device,
+//Facebook,
+Geolocation,
+Push,
+StatusBar,
+Toast,
+TouchID,
+} from 'ionic-native';
 
 @Page({
   templateUrl: 'build/pages/main/main.html',
