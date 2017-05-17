f6bdea367706596e15fae8f607b03a73f6a6a46f
Ibby Hadeed <ibby93@gmail.com>
2017-05-17 12:52:16 -0400

    updates

--- src/pages/camera-preview/camera-preview.ts
+++ src/pages/camera-preview/camera-preview.ts
@@ -1,6 +1,8 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams, Platform } from 'ionic-angular';
 import { CameraPreview, CameraPreviewOptions } from '@ionic-native/camera-preview';
+import { Base64ToGallery } from "@ionic-native/base64-to-gallery";
+import {Toast} from "@ionic-native/toast";
 
 @Component({
   selector: 'page-camera-preview',
@@ -10,12 +12,29 @@ export class CameraPreviewPage implements AfterViewInit {
 
   @ViewChild('cameraPreviewContainer') container: ElementRef;
 
+  private previewOpts: CameraPreviewOptions;
+
   constructor(
     public navCtrl: NavController
     , public navParams: NavParams
     , private platform: Platform
     , private cameraPreview: CameraPreview
+    , private base64ToGallery: Base64ToGallery
+    , private toast: Toast
   ) {}
+
+  takePicture() {
+    this.cameraPreview.takePicture({
+      width: this.previewOpts.width,
+      height: this.previewOpts.height,
+      quality: 100
+    })
+      .then(pic => this.base64ToGallery.base64ToGallery(pic))
+      .then(() => {
+        let sub = this.toast.showShortBottom('Picture taken and saved to gallery').subscribe(() => sub.unsubscribe());
+      })
+      .catch(e => console.log(e));
+  }
 
   ngAfterViewInit() {
     this.platform.ready()
@@ -24,7 +43,7 @@ export class CameraPreviewPage implements AfterViewInit {
 
         const el: HTMLElement = this.container.nativeElement;
 
-        const options: CameraPreviewOptions = {
+        const options: CameraPreviewOptions = this.previewOpts = {
           y: el.getBoundingClientRect().top,
           width: el.offsetWidth,
           height: el.offsetHeight
